233266081b233a2b2c93b04aa64d838d4f9c5fc5
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2022-10-12 22:32:17 +0200

    Add files via upload

---
+++ LeagueAPI/LeagueAPI/ChampionsRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueAPI
+{
+    internal class ChampionsRoot
+    {
+        public Dictionary<string, ChampionData> data;
+    }
+}
